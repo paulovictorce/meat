5077482b99aa0a3c59c490b27b16c368ac389946
Paulo Victor <bd.paulovictor@gmail.com>
2020-10-13 14:01:08 -0300

    Implementando o componente MenuItem.

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -6,6 +6,7 @@ import { Observable } from "rxjs/Observable";
 import { ErrorHandler } from "../app.error-handler"
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
+import { MenuItem } from "app/restaurant-detail/menu-item/menu-item.model";
 
 @Injectable()
 export class RestaurantsService {
@@ -28,4 +29,10 @@ export class RestaurantsService {
             .map(response => response.json())
             .catch(ErrorHandler.handleError);
     }
+
+    menuOfRestaurant(id: string): Observable<MenuItem[]>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError);
+    } 
 }
